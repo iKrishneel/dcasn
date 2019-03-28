bc08c247f2b43e31b159aa3231d8c11277c496ea
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2019-03-28 18:03:19 +0900

    added all models

--- handheld_object_segmentation/train/train.sh
+++ handheld_object_segmentation/train/train.sh
@@ -11,6 +11,7 @@ if [[ ! -e $dir ]]; then
     mkdir $dir
 fi
 
+export CAFFE_ROOT=/home/krishneel/caffe
 export PYTHONPATH=$CAFFE_ROOT/python:$PYTHONPATH
 pkg_directory=$PWD/..
 export PYTHONPATH=$pkg_directory/scripts/data_argumentation_layer:$PYTHONPATH
@@ -18,5 +19,5 @@ fname=$(date "+%Y-%m-%d-%H.%M-%S")
 
 $CAFFE_ROOT/build/tools/caffe train --solver=solver.prototxt \
     --gpu=0 \
-    --weights=$dir/snapshot_iter_90000.caffemodel \
+    --weights=$CAFFE_ROOT/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel \
     2>&1 | tee -a $pkg/handheld_object_$fname.log
